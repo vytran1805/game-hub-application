8bbceb899da8806607efa27563ef1e3b915a3d12
Vy Tran <vytran1805@gmail.com>
2023-05-04 19:00:32 -0400

    Build a Navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   // Create responsive layout using Grid
@@ -10,8 +11,8 @@ function App() {
         lg: `"nav nav" "aside main"`, //For large devices: >= 1024px
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       {/* Wrap this tag with <Show> to make sure Aside only displays in the large devices */}
       <Show above="lg">
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+/**
+ *
+ * @returns
+ */
+const NavBar = () => {
+  // Lay out items horizontally
+  return (
+    <HStack>
+      /* add logo */
+      <Image src={logo} boxSize="60px" />
+      <Text>NavBar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
