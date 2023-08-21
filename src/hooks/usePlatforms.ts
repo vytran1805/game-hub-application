bdd6d3c5f53f53b6e12ceab96ea95255a785eb4e
VyT <vytran1805@gmail.com>
2023-08-21 16:16:48 -0400

    Finish Creating Platforms Static Data

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,10 @@
-import useData from "./useData";
+import platforms from "../data/platforms";
 
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
-const usePlatform = () => useData<Platform>("platforms/lists/parents");
+const usePlatform = () => ({ data: platforms, isLoading: false, error: null });
 
 export default usePlatform;
