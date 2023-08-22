0dddc5d3775ff930422fbe52daf3c023774ddbd7
VyT <vytran1805@gmail.com>
2023-08-22 17:36:17 -0400

    Implement reusable logic and pass in config object in useGames hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import apiClient, { FetchResponse } from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
 import { CACHE_KEY_GAMES } from "./constants";
 import { Platform } from "./usePlatforms";
+import APIClient from "../services/api-client";
 
+// Create an APIClient instance
+const apiClient = new APIClient<Game>("games");
 export interface Game {
   id: number;
   name: string;
@@ -17,17 +20,14 @@ const useGames = (gameQuery: GameQuery) =>
   useQuery<FetchResponse<Game>, Error>({
     queryKey: [CACHE_KEY_GAMES, gameQuery], //anytime the value in this obj changes, react query will refetch the games from the backend
     queryFn: () =>
-      apiClient
-        .get<FetchResponse<Game>>("/games", {
-          //this curly bracket is a configuration obj
-          params: {
-            genres: gameQuery.genre?.id,
-            parent_platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-          },
-        })
-        .then((res) => res.data),
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+        },
+      }),
     staleTime: 24 * 60 * 60 * 1000, //24hr
   });
 
