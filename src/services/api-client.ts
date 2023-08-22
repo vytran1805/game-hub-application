6e06fbe391caba93a6a444fc2e2627fd1a8b203e
VyT <vytran1805@gmail.com>
2023-08-22 17:29:09 -0400

    Implement general/reusable HTTP requests

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,14 +1,33 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+import { GameQuery } from "../App";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api/",
   params: {
     key: "d768ac92f48f401d98848ce9f8f07594", //valid
     // key: "bf2f910a2ca54ad4845a85a13b98cc16", //invalid
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  /**
+   * Get all the data from the backend
+   * @param config Axios config obj, we need this for our games hook
+   * @returns
+   */
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+export default APIClient;
