90dfce63670457adff67bb8d44c89661f75553a7
VyT <vytran1805@gmail.com>
2023-08-22 23:39:17 -0400

    Implement Lookup hooks in the components

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -3,21 +3,17 @@ import usePlatform from "../hooks/usePlatforms";
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
 import useGenres from "../hooks/useGenres";
+import usePlatformLookup from "../hooks/usePlatformLookup";
+import useGenreLookup from "../hooks/useGenreLookup";
 
 interface Props {
   gameQuery: GameQuery;
 }
 const GameHeading = ({ gameQuery }: Props) => {
-  // get Genre name from GenreId
-  const { data: genres } = useGenres();
-  // find the g (short for genre) where g.id equals selected genreId
-  const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
-
+  // find the g (short for genre) where g.id equals given id
+  const genre = useGenreLookup(gameQuery.genreId);
   // get Platform name from PlatformId
-  const { data: platforms } = usePlatform();
-  const platform = platforms?.results.find(
-    (p) => p.id === gameQuery.platformId
-  );
+  const platform = usePlatformLookup(gameQuery.platformId);
   const heading = `${platform?.name || ""} ${genre || ""} Games`;
   return (
     <Heading as="h1" marginY={5} fontSize="5xl">
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,23 +1,24 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms, { Platform } from "../hooks/usePlatforms";
+import usePlatformLookup from "../hooks/usePlatformLookup";
 interface Props {
   // this onSelectedPlatform function takes a Platform obj and returns void
   onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: number | null;
+  selectedPlatformId?: number;
 }
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   // usePlatform() to get data dynamically and handle error
-  const { data:platforms, error } = usePlatforms();
-
-  const platform = platforms?.results.find(p=>p.id===selectedPlatform)
+  const { data: platforms, error } = usePlatforms();
+  // Find platform that its id equals given selectedPlatformId
+  const selectedPlatformObj = usePlatformLookup(selectedPlatformId);
   // if error occurs, return null
   if (error) return null;
   return (
     <Menu>
       {/* rendert as a regular Button */}
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform ? platform?.name : "Platforms"}
+        {selectedPlatformId ? selectedPlatformObj?.name : "Platforms"}
       </MenuButton>
       <MenuList>
         {platforms?.results.map((platform) => (
