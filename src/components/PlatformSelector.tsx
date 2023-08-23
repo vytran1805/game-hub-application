73c3bc426e7b8ad5bda1b5d1a12797d50c95139c
VyT <vytran1805@gmail.com>
2023-08-22 22:58:11 -0400

    Pass in PlatformId instead of Platform object

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,21 +4,23 @@ import usePlatforms, { Platform } from "../hooks/usePlatforms";
 interface Props {
   // this onSelectedPlatform function takes a Platform obj and returns void
   onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform | null;
+  selectedPlatform: number | null;
 }
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   // usePlatform() to get data dynamically and handle error
-  const { data, error } = usePlatforms();
+  const { data:platforms, error } = usePlatforms();
+
+  const platform = platforms?.results.find(p=>p.id===selectedPlatform)
   // if error occurs, return null
   if (error) return null;
   return (
     <Menu>
       {/* rendert as a regular Button */}
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform ? selectedPlatform.name : "Platforms"}
+        {selectedPlatform ? platform?.name : "Platforms"}
       </MenuButton>
       <MenuList>
-        {data?.results.map((platform) => (
+        {platforms?.results.map((platform) => (
           <MenuItem
             key={platform.id}
             onClick={() => onSelectPlatform(platform)} //notify the App component
