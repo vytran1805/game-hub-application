b7b47afaa58d39ab9faaf1ac8f73d17349cf1ab2
VyT <vytran1805@gmail.com>
2023-08-22 23:00:16 -0400

    Find Genre and Platform name with the given id

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -2,14 +2,23 @@ import React from "react";
 import usePlatform from "../hooks/usePlatforms";
 import { Heading } from "@chakra-ui/react";
 import { GameQuery } from "../App";
+import useGenres from "../hooks/useGenres";
 
 interface Props {
   gameQuery: GameQuery;
 }
 const GameHeading = ({ gameQuery }: Props) => {
-  const heading = `${gameQuery.platform?.name || ""} ${
-    gameQuery.genre?.name || ""
-  } Games`;
+  // get Genre name from GenreId
+  const { data: genres } = useGenres();
+  // find the g (short for genre) where g.id equals selected genreId
+  const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
+
+  // get Platform name from PlatformId
+  const { data: platforms } = usePlatform();
+  const platform = platforms?.results.find(
+    (p) => p.id === gameQuery.platformId
+  );
+  const heading = `${platform?.name || ""} ${genre || ""} Games`;
   return (
     <Heading as="h1" marginY={5} fontSize="5xl">
       {heading}
