4048afb08b882890acd35789da9fcba0ab40efda
VyT <vytran1805@gmail.com>
2023-08-30 16:43:00 -0400

    Remove Props, Implement Zustand store in useGenreLookup hook

--- src/hooks/useGenreLookup.ts
+++ src/hooks/useGenreLookup.ts
@@ -1,11 +1,13 @@
 import React from "react";
 import useGenres from "./useGenres";
+import useGameQueryStore from "../gameQueryStore";
 
-const useGenreLookup = (id?: number) => {
+const useGenreLookup = () => {
+  const genreId = useGameQueryStore((s) => s.gameQuery.genreId);
   // get Genre name from GenreId
   const { data: genres } = useGenres();
   // find the g (short for genre) where g.id equals given id
-  return genres?.results.find((g) => g.id === id);
+  return genres?.results.find((g) => g.id === genreId);
 };
 
 export default useGenreLookup;
